cf69e348f7901b18d0d16609ce0e63c3b64196c4
victor-shagor <ojoabiolavictor@gmail.com>
2019-05-12 11:28:46 +0100

    feature(endpoint):add get running loan [Finishes #165788518]

--- server/controllers/loans.js
+++ server/controllers/loans.js
@@ -11,6 +11,16 @@ const Loans = {
       });
     }
   },
+  getLoans(req, res) {
+    const { status, repaid } = req.query;
+    if (status === 'approved' && repaid === 'false') {
+      const loan = dbloans.filter(user => user.status === 'approved' && user.repaid === false);
+      return res.status(200).send({
+        status: 200,
+        data: loan,
+      });
+    }
+  },
 };
 
 export default Loans;
