384df63749ed4d8062cc17182a0016ac79c6b85e
victor-shagor <ojoabiolavictor@gmail.com>
2019-05-21 14:05:17 +0100

    feature(database):add-user-signin [Finishes #166109163]

--- test/userTest.js
+++ test/userTest.js
@@ -7,24 +7,6 @@ chai.use(chaiHttp);
 chai.should();
 
 describe('users', () => {
-  it('should post a user', (done) => {
-    chai.request(app)
-      .post('/api/v1/auth/signup')
-      .send({
-        firstName: 'abiola', lastName: 'ojo', email: 'adebayo12345@gmail.com', password: 'oladimeji1', address: 'no 2,lagos',
-      })
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.body.should.be.a('object');
-        res.body.should.have.property('data');
-        res.body.data.should.be.a('object');
-        res.body.data.should.have.property('firstName');
-        res.body.data.should.have.property('lastName');
-        res.body.data.should.have.property('email');
-        res.body.data.should.have.property('address');
-        done();
-      });
-  });
   it('should not create a user without a firstName ', (done) => {
     chai.request(app)
       .post('/api/v1/auth/signup')
@@ -97,7 +79,7 @@ describe('users', () => {
     chai.request(app)
       .post('/api/v1/auth/signin')
       .send({
-        email: 'adebayo12345@gmail.com', password: 'oladimeji1',
+        email: 'ojoabiola@gmail.com', password: 'oladimeji1',
       })
       .end((err, res) => {
         res.should.have.status(200);
@@ -107,5 +89,5 @@ describe('users', () => {
         res.body.data.should.have.property('lastname');
         done();
       });
-   });
+  });
 });
